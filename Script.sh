1a2ac510ebe9bf61953e36256513e840fb058fa0
Antoine ARMATOL <armatol@lyoilc16.in2p3.fr>
2019-06-14 14:16:43 +0200

    Initialisation script a adapter a votre machine

---
+++ Script.sh
@@ -0,0 +1,12 @@
+#type "source Script.sh" in the terminal
+
+#!/bin/bash
+
+source /opt/root-6.14.06/bin/thisroot.sh
+export PATH=${PATH}:~/Documents/pythia8186/bin
+export PYTHIA8DATA=`pythia8-config --xmldoc`
+
+
+
+
+
